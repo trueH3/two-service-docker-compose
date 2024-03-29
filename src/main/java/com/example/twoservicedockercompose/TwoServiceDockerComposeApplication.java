2dd4c2df058eb0ac0b717494ed5d4f27e6f232a2
Szymon <szym@MacBook-Air-Szymon.local>
2024-03-29 10:22:38 +0100

    init config

---
+++ src/main/java/com/example/twoservicedockercompose/TwoServiceDockerComposeApplication.java
@@ -0,0 +1,13 @@
+package com.example.twoservicedockercompose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoServiceDockerComposeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwoServiceDockerComposeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/twoservicedockercompose/TwoServiceDockerComposeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twoservicedockercompose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwoServiceDockerComposeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
